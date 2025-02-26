5f4b32f456ebd5fe9e416fc97a9b956d527c5e5a
Wasilii92 <eup104st@yandex.ru>
2025-02-26 15:23:06 +0500

    Домашка сделана

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,42 @@
-import java.time.LocalDate;
 
 public class Main {
-    public static void checkLeafYear  (int year){
-if ((year%4==0)&&(year%100!=0)||(year%400==0)) {
-    System.out.println(year+" год - високосный год");
-}else{System.out.println(year+" год - не високосный год");
-}
+    public static void checkYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год - високосный год");
+        } else {
+            System.out.println(year + " год - не високосный год");
+        }
     }
-    public static void checkPhone(char clientOS,int clientDeviceYear){
-        int currentYear = LocalDate.now().getYear();
-        if (clientOS == '1' && clientDeviceYear < currentYear) {
+    public static void checkPhone(char clientOS, int clientDeviceYear) {
+        if (clientOS == '1' && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию Android по ссылке");
         }
-        if (clientOS == '0' && clientDeviceYear < currentYear) {
+        if (clientOS == '0' && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию iOS по ссылке");
         }
-        if (clientOS == '1' && clientDeviceYear >= currentYear) {
+        if (clientOS == '1' && clientDeviceYear >= 2015) {
             System.out.println("Установите приложение Android по ссылке");
         }
-        if (clientOS == '0' && clientDeviceYear >= currentYear) {
+        if (clientOS == '0' && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения iOS по ссылке");
         }
     }
-public static int checkDeliveryCard(int deliveryDistance){
-        int time=0;
-    if (deliveryDistance < 20)
-        System.out.println("Потребуется дней " + (time = 1));
-    else if (deliveryDistance <= 60)
-        System.out.println("Потребуется дней " + (time = time + 1));
-    else if (deliveryDistance <= 100)
-        System.out.println("Потребуется дней " + (time = time + 2));
-    else if (deliveryDistance > 100) {
-        System.out.println("Доставки нет");
+
+    public static void checkDeliveryCard(int deliveryDistance) {
+        if (deliveryDistance < 20)
+            System.out.println("Потребуется дней 1");
+        else if (deliveryDistance <= 60)
+            System.out.println("Потребуется дней 2");
+        else if (deliveryDistance <= 100)
+            System.out.println("Потребуется дней 3");
+        else if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        }
     }
-    return time;
-}
+
     public static void main(String[] args) {
-checkLeafYear(2024);
-checkPhone('1',2025);
-checkDeliveryCard(95);
+        checkYear(2025);
+        checkPhone('1', 2025);
+        checkDeliveryCard(95);
     }
 }
